5c6b09231a140e269734b9a8ebcb5d8bece17466
Sergio <sergio.rodrigues@mercoplasa.com.br>
2024-06-12 17:55:16 -0300

    study 06/12/2024

---
+++ aula87.js
@@ -0,0 +1,45 @@
+function rand (min, max){
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo){
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') {
+            reject(false)
+            return;
+        }
+
+        setTimeout(() => {
+            resolve(msg.toUpperCase() + ' - Passei na promise')
+        }, tempo)
+    })
+}
+
+const promises = [
+    esperaAi('Promise 1', rand(1, 3)),
+    esperaAi('Promise 2', rand(1, 3)),
+    esperaAi('Promise 3', rand(1, 3)),
+]
+
+// Promise.all(promises)
+//     .then(function (valor) {
+//         console.log(valor)
+//     })
+//     .catch(function (erro) {
+//         console.log(erro)
+//     })
+
+Promise.race(promises)
+    .then(function (valor) {
+        console.log(valor)
+    })
+    .catch(function (erro) {
+        console.log(erro)
+    })
+
+
+
+
+    
